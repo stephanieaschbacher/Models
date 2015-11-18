c1b8a8ce7b4df8ef1ebd1e38c9c42047e27f9975
stephanieaschbacher <seaschbacher@comcast.net>
2015-11-17 18:07:25 -0600

    Promotion Model - test
    testing github

---
+++ Promotion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Promotion_model.Models
+{
+    public class Promotion
+    {
+        public string PromotionID { get; set;}
+        public string PromoCode { get; set; }
+        public string BookID { get; set; }
+        public decimal DiscountPrice { get; set; }
+    }
+}
